56672b3a51196c8db75e30ecc7f5dbfb4386708f
Daniel Brendel <dbrendel1988@gmail.com>
2022-04-05 22:30:02 +0200

    Add download links

--- app/views/download.php
+++ app/views/download.php
@@ -10,7 +10,7 @@
             </p>
 
             <p>
-                <a class="button is-success">Download version 1.0</a>
+                <a class="button is-success" href="{{ asset('downloads/AquaShell_v1.0_x64.zip') }}">Download version 1.0</a>
             </p>
 
             <p>
@@ -36,22 +36,22 @@
                     <tbody>
                         <tr>
                             <td><font size="5">AquaShell Plugin SDK</font><br/><font color="#646464">Use the C++ SDK to create your own native plugins</font></td>
-                            <td><a class="button is-link">Download</a></td>
+                            <td><a class="button is-link" href="{{ asset('downloads/AquaShell_PluginSDK_v1.0.zip') }}">Download</a></td>
                         </tr>
 
                         <tr>
                             <td><font size="5">AquaShell sourcecode</font><br/><font color="#646464">If you want to have a quick look. GitHub is prefered for contributing</font></td>
-                            <td><a class="button is-link">Download</a></td>
+                            <td><a class="button is-link" href="{{ asset('downloads/dnyAquaShell_v1.0_sourcecode.zip') }}">Download</a></td>
                         </tr>
 
                         <tr>
                             <td><font size="5">Notepad++ language definition</font><br/><font color="#646464">Syntax highlighting definition file for Notepad++</font></td>
-                            <td><a class="button is-link">Download</a></td>
+                            <td><a class="button is-link" href="{{ asset('downloads/dnyScript_npp_highlighting.zip') }}">Download</a></td>
                         </tr>
 
                         <tr>
                             <td><font size="5">dnyScriptInterpreter</font><br/><font color="#646464">Use the core scripting system in your own product</font></td>
-                            <td><a class="button is-link">Download</a></td>
+                            <td><a class="button is-link" href="{{ asset('downloads/dnyScriptInterpreter_v1.0.zip') }}">Download</a></td>
                         </tr>
                     </tbody>
                 </table>
