2618ab776c03aaaaebeefcb560c05a1c65c1257d
Daniel Brendel <dbrendel1988@gmail.com>
2024-03-22 15:02:53 +0100

    Update downloads section

--- app/views/download.php
+++ app/views/download.php
@@ -7,7 +7,7 @@
     </p>
 
     <p>
-        <a class="button is-success" href="{{ asset('downloads/AquaShell_v1.0_x64_Installer.zip') }}">Download version 1.0</a>
+        <a class="button is-success" href="{{ asset('downloads/AquaShell_build.zip') }}">Download version 1.0</a>
     </p>
 
     <p>
@@ -15,8 +15,8 @@
 
         <ul>
             <li>Download the ZIP archive and extract it to a location of your choice</li>
-            <li>Run the extracted installer in order to install AquaShell on your system</li>
-            <li>Run the file_association_add.bat if you want to register .dnys files with AquaShell</li>
+            <li>Run <i>dnyAquaShell.exe add_path -u</i> to add the directory of the shell to your PATH variable</li>
+            <li>The Shell is now available via <i>aquashell [args]</i></li>
             <li>Visit the <a href="{{ url('/documentation') }}">documentation</a> to see how to create scripts</li>
         </ul>
     </p>
@@ -32,28 +32,28 @@
 
             <tbody>
                 <tr>
-                    <td><font size="5">AquaShell 1.0 Portable</font><br/><font color="#646464">Portable version of AquaShell</font></td>
-                    <td><a class="button is-link" href="{{ asset('downloads/AquaShell_v1.0_x64_Portable.zip') }}">Download</a></td>
-                </tr>
-
-                <tr>
                     <td><font size="5">AquaShell Plugin SDK</font><br/><font color="#646464">Use the C++ SDK to create your own native plugins</font></td>
-                    <td><a class="button is-link" href="{{ asset('downloads/AquaShell_PluginSDK_v1.0.zip') }}">Download</a></td>
+                    <td><a class="button is-link" href="{{ asset('downloads/plugin_sdk.zip') }}">Download</a></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">AquaShell sourcecode</font><br/><font color="#646464">If you want to have a quick look. GitHub is prefered for contributing</font></td>
-                    <td><a class="button is-link" href="{{ asset('downloads/dnyAquaShell_v1.0_sourcecode.zip') }}">Download</a></td>
+                    <td><a class="button is-link" href="https://github.com/danielbrendel/dnyAquaShell">Download</a></td>
+                </tr>
+
+                <tr>
+                    <td><font size="5">VS Code Extension</font><br/><font color="#646464">Syntax highlighting for Visual Studio Code</font></td>
+                    <td><a class="button is-link" href="https://marketplace.visualstudio.com/items?itemName=danielbrendel.dnyscript-syntax">Download</a></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Notepad++ language definition</font><br/><font color="#646464">Syntax highlighting definition file for Notepad++</font></td>
-                    <td><a class="button is-link" href="{{ asset('downloads/dnyScript_npp_highlighting.zip') }}">Download</a></td>
+                    <td><a class="button is-link" href="https://github.com/aquashell-scripting/aquashell-scripting.github.io/blob/main/res/dnyScript_npp_highlighting.xml">Download</a></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">dnyScriptInterpreter</font><br/><font color="#646464">Use the core scripting system in your own product</font></td>
-                    <td><a class="button is-link" href="{{ asset('downloads/dnyScriptInterpreter_v1.0.zip') }}">Download</a></td>
+                    <td><a class="button is-link" href="https://github.com/danielbrendel/dnyScriptInterpreter">Download</a></td>
                 </tr>
             </tbody>
         </table>
