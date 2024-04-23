9cecf6e294661a87395885b8b1a69201b398f88a
Daniel Brendel <dbrendel1988@gmail.com>
2024-04-23 10:23:22 +0200

    Extension keywords & navbar mobile background coloring

--- app/views/extensions.php
+++ app/views/extensions.php
@@ -11,64 +11,81 @@
 
         <table class="other-downloads">
             <thead>
-                <tr></tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
             </thead>
 
             <tbody>
                 <tr>
                     <td><font size="5">Array</font><br/><font color="#646464">Provides array features. There are static and dynamic arrays available.</font></td>
+                    <td><span class="button is-link is-not-clickable">Language</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Auto</font><br/><font color="#646464">Provides features to automate tasks on Windows.</font></td>
+                    <td><span class="button is-warning is-not-clickable">System</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">DateTime</font><br/><font color="#646464">A datetime utility to format date and time strings.</font></td>
+                    <td><span class="button is-link is-not-clickable">Language</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">EnvVars</font><br/><font color="#646464">Exposes all environment variables to the script context.</font></td>
+                    <td><span class="button is-warning is-not-clickable">System</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Events</font><br/><font color="#646464">Allows registration and raising events.</font></td>
+                    <td><span class="button is-link is-not-clickable">Language</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">FileIO</font><br/><font color="#646464">Provides various functions to perform file system operations</font></td>
+                    <td><span class="button is-warning is-not-clickable">System</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Forms</font><br/><font color="#646464">Can be used to create and handle Windows forms for GUI scripts.</font></td>
+                    <td><span class="button is-primary is-not-clickable">UI</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">InputBox</font><br/><font color="#646464">This component allows the usage of a simple GUI input box.</font></td>
+                    <td><span class="button is-primary is-not-clickable">UI</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">IRC</font><br/><font color="#646464">A component to create IRC clients. Useful for IRC bots.</font></td>
+                    <td><span class="button is-danger is-not-clickable">Network</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">MiscUtils</font><br/><font color="#646464">Various commands that do not fit a specific category.</font></td>
+                    <td><span class="button is-warning is-not-clickable">System</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">NetClient</font><br/><font color="#646464">A component to create network clients. Supports both TCP and UDP.</font></td>
+                    <td><span class="button is-danger is-not-clickable">Network</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Speech</font><br/><font color="#646464">This component allows you to use the Microsoft SAPI.</font></td>
+                    <td><span class="button is-warning is-not-clickable">System</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">Strings</font><br/><font color="#646464">A utility component to do string manipulation.</font></td>
+                    <td><span class="button is-link is-not-clickable">Language</span></td>
                 </tr>
 
                 <tr>
                     <td><font size="5">TextInput</font><br/><font color="#646464">Used to get text input from the commandline.</font></td>
+                    <td><span class="button is-primary is-not-clickable">UI</span></td>
                 </tr>
             </tbody>
         </table>
