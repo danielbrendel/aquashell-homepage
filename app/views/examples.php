b02026a2f5d697bfd5fe43d7f5f9fa35a4279bc6
Daniel Brendel <dbrendel1988@gmail.com>
2024-04-25 02:23:15 +0200

    Table of contents

--- app/views/examples.php
+++ app/views/examples.php
@@ -4,9 +4,19 @@
     <div>
         <p>Here you will find some example scripts to give you an idea on using the shell.</p>
 
-        @foreach ($snippets as $snippet)
-            <h2>{{ $snippet->get('description') }}</h2>
+        <ul>
+            @foreach ($snippets as $snippet)
+                <li>
+                    <a href="#{{ slug($snippet->get('description')) }}">{{ $snippet->get('description') }}</a>
+                </li>
+            @endforeach
+        </ul>
 
+        @foreach ($snippets as $snippet)
+            <a name="{{ slug($snippet->get('description')) }}"></a>
+
+            <h2>{{ $snippet->get('description') }}</h2>
+            
             <pre><code class="hljs language-aquashell">{{ SnippetsModel::fixTab($snippet->get('snippet')) }}</code></pre>
         @endforeach
     </div>
