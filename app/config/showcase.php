418b2c9171d9ded1f43253249e280d3d33580cb9
Daniel Brendel <dbrendel1988@gmail.com>
2024-10-11 22:15:02 +0200

    Showcase items

--- app/config/showcase.php
+++ app/config/showcase.php
@@ -2,14 +2,14 @@
 
 return [
     [
-        'url' => 'https://www.google.de/',
-        'logo' => asset('img/logo.png'),
-        'name' => 'Google'
+        'url' => 'https://alternativeto.net/software/aquashell/about/',
+        'logo' => asset('img/alternativeto.png'),
+        'name' => 'AlternativeTo'
     ],
 
     [
-        'url' => 'https://www.google.de/',
-        'logo' => asset('img/logo.png'),
-        'name' => 'Google'
+        'url' => 'https://www.libhunt.com/r/dnyAquaShell',
+        'logo' => asset('img/libhunt.png'),
+        'name' => 'LibHunt'
     ]
 ];
