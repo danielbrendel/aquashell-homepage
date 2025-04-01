51d09ce341fe1ecdeff6101e4c25a7dfb9819180
Daniel Brendel <dbrendel1988@gmail.com>
2025-04-01 22:32:51 +0200

    Same here

--- app/views/download.php
+++ app/views/download.php
@@ -85,7 +85,7 @@
             <div class="info-list-item">
                 <div class="info-list-header">
                     <div class="info-list-header-title">AquaHotS: Chat Hotkey Manager</div>
-                    <div class="info-list-header-button"><a class="button is-link" href="https://github.com/danielbrendel/aquashell-hots">Download</a></div>
+                    <div class="info-list-header-button"><a class="button is-link" href="https://github.com/danielbrendel/aquahots-tool">Download</a></div>
                 </div>
 
                 <div class="info-list-content">A Hotkey Chat Message Manager for Heroes of the Storm</div>
