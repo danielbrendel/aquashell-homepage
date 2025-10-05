fa69332022b770efff4e7865e94fa9e5f85c6e06
Daniel Brendel <dbrendel1988@gmail.com>
2025-10-05 15:54:01 +0200

    Include snippet links in sitemap

--- app/modules/SitemapModule.php
+++ app/modules/SitemapModule.php
@@ -23,6 +23,10 @@ class SitemapModule {
                 $sitemap[] = '/tutorials';
             }
 
+            foreach (SnippetsModel::getAll() as $snippet) {
+                $sitemap[] = '/snippets/show/' . $snippet->get('id') . '/' . slug($snippet->get('title'));
+            }
+
             $xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{%URLS%}</urlset>';
             $node = '<url><loc>{%URL%}</loc></url>';
 
